19154c9be599d1e51dd7df2c514dc1bb812cef08
Weida Xue <luckyweida@gmail.com>
2017-05-10 19:54:49 +1200

    francis chagnes

--- wp-content/themes/natureandfresh/the-photo.php
+++ wp-content/themes/natureandfresh/the-photo.php
@@ -8,23 +8,22 @@ Template Name: The photo
 
 
 <div id="the-photo" class="container">
-    <div class="container-fluid article-offset">
-        <div class="row">
-            <div class="col-md-5 col-xs-12">
-                <h1>The photo</h1>
-            </div>
-            <div class="col-md-offset-1 col-md-6 col-xs-12 we-keep-things-simpl">
-                <p>We keep things simple to keep our farm beautiful, our farm relies on fertilizers of organic origin and is 100% pesticide free. Write something about happy animal and showing user photos. Lorem ipsum lorem ipsum  lorem.</p>
-            </div>
-        </div>
 
-        <div id="my-photos" class="row">
+    <div class="row page-description">
+        <div class="col-md-5 col-sm-12">
+            <h4>The photo</h4>
         </div>
+        <div class="col-md-7 col-sm-12">
+            <p class="text">We keep things simple to keep our farm beautiful, our farm relies on fertilizers of organic origin and is 100% pesticide free. Write something about happy animal and showing user photos. Lorem ipsum lorem ipsum  lorem.</p>
+        </div>
+    </div>
 
-        <div class="row">
-            <div class="col-lg-12 col-centered">
-                <button class="load-more btn btn-lg btn-block btn-primary" style="display: none;">Load more</button>
-            </div>
+    <div id="my-photos" class="row">
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 col-centered">
+            <button class="load-more btn btn-sm" style="display: none;">Load more</button>
         </div>
     </div>
 </div>
