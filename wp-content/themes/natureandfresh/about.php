cddc8052ed8b7009681bfaaf3485dba10b2e5954
Weida Xue <luckyweida@gmail.com>
2017-06-21 16:39:43 +1200

    Francis made changes

--- wp-content/themes/natureandfresh/about.php
+++ wp-content/themes/natureandfresh/about.php
@@ -14,36 +14,33 @@ Template Name: About
 
 			<div class="article-offset_img col-md-6 col-xs-12 noPadding">
 				<div class="row">
+					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
+						<img data-delay="600" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_tree.jpg">
+					</div>
 					<div class="col-md-6 col-xs-12 noPadding">
 						<img data-delay="300" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_pig.jpg">
 					</div>
 					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
-						<img data-delay="600" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_tree.jpg">
+						<img data-delay="1500" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_chicken.jpg">
 					</div>
+					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
+						<img data-delay="600" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_wood.jpg">
+					</div>
+					<!-- <div class="col-md-6 col-xs-12 noPadding hidden-xs">
+						<img data-delay="600" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_tool.jpg">
+					</div> -->
 
-					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
-						<img data-delay="600" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_tree.jpg">
-					</div>
-					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
-						<img data-delay="900" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_pig.jpg">
-					</div>
-
-					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
-						<img data-delay="1500" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_pig.jpg">
-					</div>
-					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
-						<img data-delay="1800" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_tree.jpg">
-					</div>
+					
 				</div>
 			</div>
 
 			<article class="col-md-6 col-xs-12">
 				<div class="article-offset_heading">
-					<h2 class="article-title">Organic &amp; Natural from groud upin Franklin, Waiuku</h2>
+					<h2 class="article-title">Organic &amp; Natural from groud up in Franklin, Waiuku</h2>
 				</div>
 				<p>Nature &amp; Fresh’s twelve acre orchard is 6 years old and situated in the Franklin suburb of Waiuku. With 700 peak age macadamia trees, over 150 feijoa trees, 100 chestnut trees, and 200 freshly planted Manuka, we are diverse yet focussed on our greatest prize: the macadamia.</p>
 				<p>We have a strong organic approach, which includes the way we raise and utilise our animals.  Over 100 chickens, ducks and geese roam the farm, keeping the number of insects and weeds in the soil to a minimum. Also among the Nature &amp; Fresh family are sheep and pigs, who eat the macadamia nuts that have fallen to the ground. This keeps the farm tidy in a cheap and environmentally friendly way while maintaining healthy animals and a positive, natural cycle.</p>
-				
+
 			</article>
 
 		</div>
@@ -58,6 +55,8 @@ Template Name: About
 			<article class="col-md-6 col-xs-12">
 				
 				<h2 class="article-title">We're all about growing the perfect macadamia nuts</h2>
+				
+
 				<p>Our farm is spray-free and we try our best not to use machines or pesticides in our orchard. Instead, we use natural fertilizers such as Agrissentials Rok Solid and comfrey. We use comfrey as a liquid fertilizer on our trees for the nutrients and minerals it brings up from deep in the ground.</p>
 				<p>We are out on the farm daily picking the macadamia nuts which are then husked and dried for eight weeks. The cracked nuts are baked at a low temperature then vacuum packed to preserve that fresh, original macadamia taste that we love. From growing to harvesting, drying and packaging, you can enjoy natural quality foods that have been grown and prepared with care.</p>
 				<p>With more products to come, it is an exciting time for everyone at Nature &amp; Fresh! Stay tuned to our website for more updates and information as we continue to work on bringing you more of the fresh, natural taste you love.
@@ -66,13 +65,22 @@ Template Name: About
 
 			<div class="article-offset_img col-md-6 col-xs-12 noPadding">
 				<div class="row">
-					<div class="col-md-12 col-xs-12 noPadding">
-						<img data-delay="300" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_pig.jpg">
+					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
+						<img data-delay="1800" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_nutontree.jpg">
 					</div>
-					<div class="col-md-12 col-xs-12 noPadding hidden-xs">
-						<img data-delay="600" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_tree.jpg">
+					<div class="col-md-6 col-xs-12 noPadding">
+						<img data-delay="300" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_nuthand1.jpg">
+					</div>
+					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
+						<img data-delay="600" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_nutfilter.jpg">
+					</div>
+
+					<div class="col-md-6 col-xs-12 noPadding hidden-xs">
+						<img data-delay="1500" class="lazy-load" data-src="<?php echo get_template_directory_uri(); ?>/images/pages/about_nutbag.jpg">
 					</div>
 				</div>
+
+				
 			</div>
 
 		</div>
